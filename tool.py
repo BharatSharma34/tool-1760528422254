41f4fff4942d78c17fa1d703b36436f325f83b2b
BharatSharma34 <bharatsharma@ducktaleit.com>
2025-10-15 17:10:24 +0530

    Create tool.py

---
+++ tool.py
@@ -0,0 +1,31 @@
+"""
+Tool - Custom Lambda Tool
+Description: fdger
+
+IMPORTANT: Only edit the code in the main() function below.
+The Lambda handler will be automatically appended during deployment.
+DO NOT add lambda_handler code here - it will be added automatically.
+"""
+
+def main(event_body):
+    """
+    Main function for Tool
+    This function will be called by the Lambda handler.
+    
+    Parameters:
+    event_body: dict - The request body passed to the Lambda function
+    
+    Returns:
+    dict - JSON-serializable response
+    """
+    # Your tool logic here
+    # Access parameters from event_body
+    # Example: user_input = event_body.get('input_data', '')
+    
+    return {
+        "success": True,
+        "message": "Hello from Tool!",
+        "data": event_body
+    }
+
+# You can add helper functions below
